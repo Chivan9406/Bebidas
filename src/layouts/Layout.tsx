45d45185868527e73fb54da2fc9f40563be73ce7
Chivan9406 <ivan9406@hotmail.com>
2025-09-05 11:52:25 -0600

    toastify

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router'
 import { useEffect } from 'react'
+import { ToastContainer } from 'react-toastify'
 
 import Header from '../components/Header.tsx'
 import Modal from '../components/Modal.tsx'
 import { useAppStore } from '../stores/useAppStore.ts'
-import Notification from '../components/Notification.tsx'
 
 function Layout() {
   const loadFromStorage = useAppStore(state => state.loadFromStorage)
@@ -17,7 +17,7 @@ function Layout() {
         <Outlet />
       </main>
       <Modal />
-      <Notification />
+      <ToastContainer />
     </>
   )
 }
--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -1,44 +1,23 @@
 import type { StateCreator } from 'zustand'
 
 import type { FavoritesSliceType } from './favoritesSlice.ts'
+import { toast } from 'react-toastify'
 
 type Notification = {
   text: string
   error: boolean
-  show: boolean
 }
 
 export type NotificationSliceType = {
-  notification: Notification
-  showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void
-  hideNotification: () => void
+  showNotification: (payload: Notification) => void
 }
 
-export const createNotificationSlice: StateCreator<NotificationSliceType & FavoritesSliceType, [], [], NotificationSliceType> = (set, get) => ({
-  notification: {
-    text: '',
-    error: false,
-    show: false
-  },
+export const createNotificationSlice: StateCreator<NotificationSliceType & FavoritesSliceType, [], [], NotificationSliceType> = () => ({
   showNotification: (payload) => {
-    set({
-      notification: {
-        text: payload.text,
-        error: payload.error,
-        show: true
-      }
-    })
-    setTimeout(() => {
-      get().hideNotification()
-    }, 5000)
-  },
-  hideNotification: () => {
-    set({
-      notification: {
-        text: '',
-        error: false,
-        show: false
-      }
-    })
+    if (payload.error) {
+      toast.error(payload.text)
+    } else {
+      toast.success(payload.text)
+    }
   }
 })
